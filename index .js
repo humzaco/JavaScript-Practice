a6d233895ac368d1d2a045fcf0a4dd2c5afb1aca
Humza <109144839+humzaco@users.noreply.github.com>
2023-03-07 11:12:24 +0500

    Truthy and falsy values

--- index .js
+++ index .js
@@ -53,3 +53,19 @@ console.log(aboutMe);
 // != vs !==
 
 // console.log(num1 !== num2);
+
+
+
+
+
+
+// truthy and falsy values 
+
+// falsy values 
+
+
+// false
+// ""
+// null 
+// undefined
+// 0
