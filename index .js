ef0f60b55502619a8fb11c33d8ca6eb37d70fdde
Humza <109144839+humzaco@users.noreply.github.com>
2023-03-10 10:35:38 +0500

    if else if

--- index .js
+++ index .js
@@ -192,15 +192,47 @@
 // 17 too low 
 // 20 too high 
 
-let winningNumber = 19;
-let userGuess = +prompt("Guess a number");
+// let winningNumber = 19;
+// let userGuess = +prompt("Guess a number");
 
-if(userGuess === winningNumber){
-    console.log("Your guess is right!!");
-}else{
-    if(userGuess < winningNumber){
-        console.log("too low !!!");
-    }else{
-        console.log("too high !!!");
-    }
-}
+// if(userGuess === winningNumber){
+//     console.log("Your guess is right!!");
+// }else{
+//     if(userGuess < winningNumber){
+//         console.log("too low !!!");
+//     }else{
+//         console.log("too high !!!");
+//     }
+// }
+
+
+
+
+
+
+
+
+// if 
+// else if 
+// else if 
+// else if 
+// else 
+
+// let tempInDegree = 50;
+
+
+// if(tempInDegree < 0){
+//     console.log("extremely cold outside");
+// }else if(tempInDegree < 16){
+//     console.log("It is cold outside");
+// }else if(tempInDegree < 25){
+//     console.log("wheather is okay ");
+// }else if(tempInDegree < 35){
+//     console.log("lets go for swim");
+// }else if(tempInDegree < 45){
+//     console.log("turn on AC");
+// }else{
+//     console.log("too hot!!");
+// }
+
+// console.log("hello");
