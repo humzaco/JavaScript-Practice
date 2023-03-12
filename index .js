4811b2fd06b86e21559d26f81853a0be79a0ee42
Humza <109144839+humzaco@users.noreply.github.com>
2023-03-12 15:04:17 +0500

    for loop

--- index .js
+++ index .js
@@ -344,3 +344,17 @@
 // }
 
 // console.log("value of i is ",i);
+
+
+
+// for loop example 
+
+// let total = 0;
+
+// let num = 100;
+
+// for(let i = 1; i<=num; i++){
+//     total = total + i;
+// }
+
+// console.log(total);
