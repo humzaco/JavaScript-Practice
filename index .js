67b48d5f0f694edaeffcfaddeaa83233fa2ef80b
Humza <109144839+humzaco@users.noreply.github.com>
2023-03-12 15:02:40 +0500

    Intro to for loop

--- index .js
+++ index .js
@@ -334,3 +334,13 @@
 
 // let total = (num*(num+1))/2;
 // console.log(total);
+
+
+// intro to for loop 
+// print 0 to 9
+
+// for(let i = 0;i<=9;i++){
+//     console.log(i);
+// }
+
+// console.log("value of i is ",i);
