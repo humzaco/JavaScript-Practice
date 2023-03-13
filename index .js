e2b889453df10efe04654fcbc572b3b21fca4d46
Humza <109144839+humzaco@users.noreply.github.com>
2023-03-13 12:11:01 +0500

    do while loop

--- index .js
+++ index .js
@@ -381,3 +381,21 @@
 //     console.log(i);
 // }
 // console.log("hello there");
+
+
+
+
+// do while loop
+
+// while(i<=9){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 10;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=9);
+
+// console.log("value of i is ", i);
